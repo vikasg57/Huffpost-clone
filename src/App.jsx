5577fb247829fb0eae62b52a47adf463c43ec6d1
Vikas Gaikwad <vgaikwad299@gmail.com>
2022-03-29 11:57:21 +0530

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import './App.css';
 
+import  {Navbar}  from './components/navbar/Navbar';
 function App() {
   return (
     <div className="App">
+
+      <Navbar/>
+
+     
       <h1>Hello Huffpost  </h1>
       <h1>Hello World</h1>
       <h1>new branch created</h1>
