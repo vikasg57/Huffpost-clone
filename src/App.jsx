6a1127c0fc84446bebf7bd0e2aec6d709085968c
TriveniprasadGupta <iam.trivenigupta@gmail.com>
2022-03-29 12:15:32 +0530

    init NewContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
+import { NewsContainer } from './components/NewsContainer/NewsContainer';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      
+
+      <NewsContainer />
+
     </div>
   );
 }
---
+++ src/components/NewsContainer/NewsContainer.jsx
@@ -0,0 +1,7 @@
+
+
+export const NewsContainer = ()=>{
+    return <div>
+        <h1>New Container</h1>
+    </div>
+}
