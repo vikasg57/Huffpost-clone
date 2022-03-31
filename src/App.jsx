77c29734cab4d2c5f6594b7bfeda5635d53b7454
Vikas Gaikwad <vgaikwad299@gmail.com>
2022-03-31 21:07:19 +0530

    app.js upadted

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 import  {Navbar}  from './components/navbar/Navbar';
 import {Footer} from "./components/footer/Footer"
-import{HiX} from "react-icons/hi"
+
 import{motion} from"framer-motion"
 import{Sidebar} from "./components/sidebar/Sidebar"
 
@@ -14,18 +14,16 @@ function App() {
    }
 
   return (
-    <div className="App"
-    >
-
-      <Sidebar sidebar={sidebar} togglestate={togglestate}/>
-      <div className="App__child">
-      <Navbar state={togglestate}/>       
-      <Footer/>
-      </div>
+    <div className="App">
 
 
-     
       
+       <Sidebar sidebar={sidebar} togglestate={togglestate}/>
+       <div className={sidebar?"App__child":"App__child child__width" }>
+       <Navbar state={togglestate}/>
+       <Footer/>
+      </div> 
+
     </div>
   );
 }
